b9cfc769ec6f9de06642e5441a243a315ee59c14
Alex Gordon <alexgordon@alex-pc.config>
2009-12-15 19:24:19 +0000

    Xcode project

---
+++ Helium_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  Helium_AppDelegate.h
+//  Helium
+//
+//  Created by Alex Gordon on 15/12/2009.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Helium_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Helium
+//
+//  Created by Alex Gordon on 15/12/2009.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
